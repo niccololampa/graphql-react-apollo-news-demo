e472de1bd843f0e70476ac9cbefab744b465e819
niccololampa <niccololampa@gmail.com>
2019-12-22 18:57:25 +0800

    Add search and filtering

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,14 @@ class Header extends Component {
       <div className="flex pa1 justify-between nowrap orange">
         <div className="flex flex-fixed black">
           <div className="fw7 mr1">Hacker News</div>
+          <Link to="/search" className="ml1 no-underline black">
+            search
+          </Link>
+          <div className="ml1">|</div>
           <Link to="/" className="ml1 no-underline black">
             new
           </Link>
+
           {authToken && (
             <div className="flex">
               <div className="ml1">|</div>
