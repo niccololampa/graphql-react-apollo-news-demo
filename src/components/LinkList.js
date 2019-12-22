f067d70c1aaaa7405dc890b6cf7eef2119193731
niccololampa <niccololampa@gmail.com>
2019-12-22 19:29:58 +0800

    Add graphql subscriptions via websocket

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -3,6 +3,54 @@ import Link from "./Link";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 export const FEED_QUERY = gql`
   {
     feed {
@@ -35,6 +83,32 @@ class LinkList extends Component {
     store.writeQuery({ query: FEED_QUERY, data });
   };
 
+  _subscribeToNewLinks = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_LINKS_SUBSCRIPTION,
+      updateQuery: (prev, { subscriptionData }) => {
+        if (!subscriptionData.data) return prev;
+        const newLink = subscriptionData.data.newLink;
+        const exists = prev.feed.links.find(({ id }) => id === newLink.id);
+        if (exists) return prev;
+
+        return Object.assign({}, prev, {
+          feed: {
+            links: [newLink, ...prev.feed.links],
+            count: prev.feed.links.length + 1,
+            __typename: prev.feed.__typename
+          }
+        });
+      }
+    });
+  };
+
+  _subscribeToNewVotes = subscribeToMore => {
+    subscribeToMore({
+      document: NEW_VOTES_SUBSCRIPTION
+    });
+  };
+
   render() {
     // const linksToRender = [
     //   {
@@ -51,11 +125,14 @@ class LinkList extends Component {
 
     return (
       <Query query={FEED_QUERY}>
-        {({ loading, error, data }) => {
+        {({ loading, error, data, subscribeToMore }) => {
           if (loading) return <div>Fetching</div>;
           if (error) return <div>Error</div>;
 
           const linksToRender = data.feed.links;
+
+          this._subscribeToNewLinks(subscribeToMore);
+          this._subscribeToNewVotes(subscribeToMore);
 
           return (
             <div>
